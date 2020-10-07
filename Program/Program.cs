5a9c09ca262c60dae3e6a05e16d88f918e3fec7a
Miles Watson <milestwatson@icloud.com>
2020-10-07 22:31:42 +0100

    Tested socket connect / accept / send / receive.

--- Program/Program.cs
+++ Program/Program.cs
@@ -13,22 +13,23 @@ namespace Hosta
 	{
 		public static void Main()
 		{
-			/*
 			using var socketServer = new SocketServer(11000);
 			var accept = socketServer.Accept();
 
 			var socketClient = new SocketClient();
-			using SocketMessenger requester = socketClient.Connect(socketServer.Address, socketServer.Port).Result;
+			using SocketMessenger requester = socketClient.Connect(socketServer.address, socketServer.port).Result;
 			requester.Send(new byte[] { 0, 1, 52 }).Wait();
 
 			using SocketMessenger handler = accept.Result;
 
 			Console.WriteLine(string.Join(",", handler.Receive().Result.Select(o => o.ToString()).ToArray()));
-			*/
+
+			/*
 			var start = new byte[] { 1 };
 			AesCrypter aes = new AesCrypter(SecureRandom.GetBytes(32));
 			var finish = aes.Decrypt(aes.Encrypt(start));
 			Console.WriteLine(string.Join(",", finish.Select(o => o.ToString()).ToArray()));
+			*/
 		}
 	}
 }
