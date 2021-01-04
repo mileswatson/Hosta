6cf2f2e4564154304a3bfbce9a562fa60659217b
Miles Watson <milestwatson@icloud.com>
2021-01-04 22:14:04 +0000

    Merged Startup and Connect views, added IP/port validation + parsing.

--- src/ClientWPF/ViewModels/StartupViewModel.cs
+++ src/ClientWPF/ViewModels/StartupViewModel.cs
@@ -1,19 +1,21 @@
-﻿using System;
+﻿using static ClientWPF.ApplicationEnvironment;
+using System;
 using System.ComponentModel;
+using System.Net;
 using System.Windows.Input;
 
 namespace ClientWPF.ViewModels
 {
 	public class StartupViewModel : INotifyPropertyChanged
 	{
-		public ICommand FolderSelected { get; private set; }
+		public ICommand Continue { get; private set; }
 		public ICommand Quit { get; private set; }
 
 		private string folder = "folder path";
 
 		public string Folder
 		{
-			get { return folder; }
+			get => folder;
 			set
 			{
 				folder = value;
@@ -21,9 +23,71 @@ namespace ClientWPF.ViewModels
 			}
 		}
 
-		public StartupViewModel(Action<string> OnFolderSelected, Action OnQuit)
+		private string ip = "";
+
+		public string IP
 		{
-			FolderSelected = new RelayCommand((object? _) => OnFolderSelected(Folder));
+			get => ip;
+			set
+			{
+				ip = value;
+				NotifyPropertyChanged(nameof(IP));
+			}
+		}
+
+		private string port = "";
+
+		public string Port
+		{
+			get => port;
+			set
+			{
+				port = value;
+				NotifyPropertyChanged(nameof(Port));
+			}
+		}
+
+		public StartupViewModel(Action<string, IPEndPoint> OnConnect, Action OnQuit)
+		{
+			Continue = new RelayCommand((object? _) =>
+			{
+				string folder = Folder;
+
+				// Ensure that directory exists
+				if (!CheckDirectory(folder))
+				{
+					Alert("Folder does not exist!");
+					return;
+				}
+
+				// Ensure that the IP address is valid
+				IPAddress address;
+				try
+				{
+					address = IPAddress.Parse(IP.Trim());
+				}
+				catch
+				{
+					Alert("The IP field was in an invalid format!");
+					return;
+				}
+
+				// Ensure that the port is an integer, and in the valid range
+				int port;
+				try
+				{
+					port = int.Parse(Port.Trim());
+					if (port < IPEndPoint.MinPort || port > IPEndPoint.MaxPort)
+						throw new Exception();
+				}
+				catch
+				{
+					Alert("The port field was in an invalid format!");
+					return;
+				}
+
+				OnConnect(folder, new IPEndPoint(address, port));
+			});
 			Quit = new RelayCommand((object? _) => OnQuit());
 		}
 
--- src/ClientWPF/ViewModels/ViewModel.cs
+++ src/ClientWPF/ViewModels/ViewModel.cs
@@ -3,13 +3,13 @@ using Hosta.Tools;
 using System;
 using System.ComponentModel;
 using System.IO;
+using System.Net;
 using static ClientWPF.ApplicationEnvironment;
 
 namespace ClientWPF.ViewModels
 {
 	public class ViewModel : INotifyPropertyChanged
 	{
-		private readonly ConnectViewModel connect;
 		private readonly StartupViewModel startup;
 
 		private object _vm;
@@ -29,44 +29,35 @@ namespace ClientWPF.ViewModels
 
 		public ViewModel()
 		{
-			connect = new ConnectViewModel(OnConnect, OnChangeProfile);
-			_vm = startup = new StartupViewModel(OnFolderSelected, OnQuit);
+			_vm = startup = new StartupViewModel(OnConnect, OnQuit);
 			Transcoder.BytesFromHex("A4");
 		}
 
 		public void OnQuit() => Environment.Exit(0);
 
-		public async void OnFolderSelected(string folder)
+		public async void OnConnect(string folder, IPEndPoint endpoint)
 		{
-			if (!CheckDirectory(folder))
-			{
-				Alert("Folder does not exist!");
-				return;
-			}
 			var file = Path.Combine(folder, "client.identity");
+			PrivateIdentity identity;
 			if (!CheckFile(file))
 			{
 				if (!Confirm($"No identity was found at {file}. Do you want to create a new one?"))
 					return;
-				var privateIdentity = PrivateIdentity.Create();
-				var hex = Transcoder.HexFromBytes(PrivateIdentity.Export(privateIdentity));
+				identity = PrivateIdentity.Create();
+				var hex = Transcoder.HexFromBytes(PrivateIdentity.Export(identity));
 				await WriteFile(file, hex);
-				Alert($"Identity {privateIdentity.ID} has been created and saved.");
+				Alert($"Identity {identity.ID} has been created and saved.");
 			}
 			else
 			{
 				var hex = Transcoder.BytesFromHex(await ReadFile(file));
-				var privateIdentity = PrivateIdentity.Import(hex);
-				Alert($"Loaded identity {privateIdentity.ID}.");
+				identity = PrivateIdentity.Import(hex);
 			}
-			VM = connect;
+			Alert($"Loaded {identity.ID} from {folder}, will connect to {endpoint}");
+			VM = new ConnectedViewModel(OnDisconnect);
 		}
 
-		public void OnChangeProfile() => VM = startup;
-
-		public void OnConnect() => VM = new ConnectedViewModel(OnDisconnect);
-
-		public void OnDisconnect() => VM = connect;
+		public void OnDisconnect() => VM = startup;
 
 		public event PropertyChangedEventHandler? PropertyChanged;
 
