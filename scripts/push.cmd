14c479d451c99498ff03ad8e741345d7214ada72
Miles Watson <milestwatson@icloud.com>
2021-02-15 17:41:38 +0000

    Added script to push to docker hub.

---
+++ scripts/push.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+powershell write-host -fore Yellow "This command should be run from the root Hosta directory. Ensure that you are in the correct directory before continuing."
+pause
+
+powershell write-host -fore Yellow "Are you sure you want to set the version to" v%1"?"
+pause
+
+powershell write-host -fore Cyan "Building and pushing..."
+cd src
+docker buildx build -f Node/Dockerfile --platform linux/amd64,linux/arm64,linux/arm -t mileswatson/hosta:node -t mileswatson/hosta:node-v%1 . --push
